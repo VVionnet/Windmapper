796ac49f565ec69c88fe22f956c6d4f624bcf44e
Chris Marsh <chrismarsh.c2@gmail.com>
2020-07-02 21:18:01 -0600

    fix exit command

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     pyenv activate Windmapper
 fi
 
-if [ "$test_conda" = "1" ]; then exit(0) ; fi
+if [ "$test_conda" = "1" ]; then exit 0 ; fi
 
 pip install twine
 pip install conan
@@ -16,9 +16,8 @@ pip install ninja
 pip install wheel
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  python setup.py sdist #bdist_wheel
+  python setup.py sdist 
 else
-  python setup.py sdist #no binary wheels on linux at the moment as we link against non PEP0513 .so
-  #https://www.python.org/dev/peps/pep-0513/
+  python setup.py sdist 
 fi
 twine upload  --skip-existing dist/*
